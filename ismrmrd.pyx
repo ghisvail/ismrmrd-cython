5bd5ab5481cac747e07e3e307a0ca72f34143f45
Ghislain Antony Vaillant <ghisvail@gmail.com>
2014-10-20 18:18:23 +0100

    make consistent usage of this as wrapper ptr

--- ismrmrd.pyx
+++ ismrmrd.pyx
@@ -24,50 +24,50 @@ cdef class EncodingCounters:
         free(self.this)
 
     property kspace_encode_step_1:
-        def __get__(self): return self.thisptr.kspace_encode_step_1
-        def __set__(self, val): self.thisptr.kspace_encode_step_1 = val
+        def __get__(self): return self.this.kspace_encode_step_1
+        def __set__(self, val): self.this.kspace_encode_step_1 = val
 
     property kspace_encode_step_2:
-        def __get__(self): return self.thisptr.kspace_encode_step_2
-        def __set__(self, val): self.thisptr.kspace_encode_step_2 = val
+        def __get__(self): return self.this.kspace_encode_step_2
+        def __set__(self, val): self.this.kspace_encode_step_2 = val
 
     property average:
-        def __get__(self): return self.thisptr.average
-        def __set__(self, val): self.thisptr.average = val
+        def __get__(self): return self.this.average
+        def __set__(self, val): self.this.average = val
 
     property slice:
-        def __get__(self): return self.thisptr.slice
-        def __set__(self, val): self.thisptr.slice = val
+        def __get__(self): return self.this.slice
+        def __set__(self, val): self.this.slice = val
 
     property contrast:
-        def __get__(self): return self.thisptr.contrast
-        def __set__(self, val): self.thisptr.contrast = val
+        def __get__(self): return self.this.contrast
+        def __set__(self, val): self.this.contrast = val
 
     property phase:
-        def __get__(self): return self.thisptr.phase
-        def __set__(self, val): self.thisptr.phase = val
+        def __get__(self): return self.this.phase
+        def __set__(self, val): self.this.phase = val
 
     property repetition:
-        def __get__(self): return self.thisptr.repetition
-        def __set__(self, val): self.thisptr.repetition = val
+        def __get__(self): return self.this.repetition
+        def __set__(self, val): self.this.repetition = val
 
     property set:
-        def __get__(self): return self.thisptr.set
-        def __set__(self, val): self.thisptr.set = val
+        def __get__(self): return self.this.set
+        def __set__(self, val): self.this.set = val
 
     property segment:
-        def __get__(self): return self.thisptr.segment
-        def __set__(self, val): self.thisptr.segment = val
+        def __get__(self): return self.this.segment
+        def __set__(self, val): self.this.segment = val
 
     property user:
         def __get__(self):
             l = [0 for i in range(cismrmrd.ISMRMRD_USER_INTS)]
             for i in range(cismrmrd.ISMRMRD_USER_INTS):
-                l[i] = self.thisptr.user[i]
+                l[i] = self.this.user[i]
             return l
         def __set__(self, vals):
             for i in range(cismrmrd.ISMRMRD_USER_INTS):
-                self.thisptr.user[i] = vals[i]
+                self.this.user[i] = vals[i]
 
 cdef class AcquisitionHeader:
     cdef cismrmrd.ISMRMRD_AcquisitionHeader *this
@@ -81,112 +81,112 @@ cdef class AcquisitionHeader:
         def __set__(self, val): self.this.version = val
 
     property flags:
-        def __get__(self): return self.thisptr.flags
-        def __set__(self, val): self.thisptr.flags = val
+        def __get__(self): return self.this.flags
+        def __set__(self, val): self.this.flags = val
 
     property measurement_uid:
-        def __get__(self): return self.thisptr.measurement_uid
-        def __set__(self, val): self.thisptr.measurement_uid = val
+        def __get__(self): return self.this.measurement_uid
+        def __set__(self, val): self.this.measurement_uid = val
 
     property scan_counter:
-        def __get__(self): return self.thisptr.scan_counter
-        def __set__(self, val): self.thisptr.scan_counter = val
+        def __get__(self): return self.this.scan_counter
+        def __set__(self, val): self.this.scan_counter = val
 
     property acquisition_time_stamp:
-        def __get__(self): return self.thisptr.acquisition_time_stamp
-        def __set__(self, val): self.thisptr.acquisition_time_stamp = val
+        def __get__(self): return self.this.acquisition_time_stamp
+        def __set__(self, val): self.this.acquisition_time_stamp = val
 
     property physiology_time_stamp:
         def __get__(self):
-            return [self.thisptr.physiology_time_stamp[i] for i in
+            return [self.this.physiology_time_stamp[i] for i in
                     range(cismrmrd.ISMRMRD_PHYS_STAMPS)]
         def __set__(self, val):
             for i in range(cismrmrd.ISMRMRD_PHYS_STAMPS):
-                self.thisptr.physiology_time_stamp[i] = val[i]
+                self.this.physiology_time_stamp[i] = val[i]
 
     property number_of_samples:
-        def __get__(self): return self.thisptr.number_of_samples
-        def __set__(self, val): self.thisptr.number_of_samples = val
+        def __get__(self): return self.this.number_of_samples
+        def __set__(self, val): self.this.number_of_samples = val
 
     property available_channels:
-        def __get__(self): return self.thisptr.available_channels
-        def __set__(self, val): self.thisptr.available_channels = val
+        def __get__(self): return self.this.available_channels
+        def __set__(self, val): self.this.available_channels = val
 
     property active_channels:
-        def __get__(self): return self.thisptr.active_channels
-        def __set__(self, val): self.thisptr.active_channels = val
+        def __get__(self): return self.this.active_channels
+        def __set__(self, val): self.this.active_channels = val
 
     property channel_mask:
         def __get__(self):
-            return [self.thisptr.channel_mask[i] for i in
+            return [self.this.channel_mask[i] for i in
                     range(cismrmrd.ISMRMRD_CHANNEL_MASKS)]
         def __set__(self, val):
             for i in range(cismrmrd.ISMRMRD_CHANNEL_MASKS):
-                self.thisptr.channel_mask[i] = val[i]
+                self.this.channel_mask[i] = val[i]
 
     property discard_pre:
-        def __get__(self): return self.thisptr.discard_pre
-        def __set__(self, val): self.thisptr.discard_pre = val
+        def __get__(self): return self.this.discard_pre
+        def __set__(self, val): self.this.discard_pre = val
 
     property discard_post:
-        def __get__(self): return self.thisptr.discard_post
-        def __set__(self, val): self.thisptr.discard_post = val
+        def __get__(self): return self.this.discard_post
+        def __set__(self, val): self.this.discard_post = val
 
     property center_sample:
-        def __get__(self): return self.thisptr.center_sample
-        def __set__(self, val): self.thisptr.center_sample = val
+        def __get__(self): return self.this.center_sample
+        def __set__(self, val): self.this.center_sample = val
 
     property encoding_space_ref:
-        def __get__(self): return self.thisptr.encoding_space_ref
-        def __set__(self, val): self.thisptr.encoding_space_ref = val
+        def __get__(self): return self.this.encoding_space_ref
+        def __set__(self, val): self.this.encoding_space_ref = val
 
     property trajectory_dimensions:
-        def __get__(self): return self.thisptr.trajectory_dimensions
-        def __set__(self, val): self.thisptr.trajectory_dimensions = val
+        def __get__(self): return self.this.trajectory_dimensions
+        def __set__(self, val): self.this.trajectory_dimensions = val
 
     property sample_time_us:
-        def __get__(self): return self.thisptr.sample_time_us
-        def __set__(self, val): self.thisptr.sample_time_us = val
+        def __get__(self): return self.this.sample_time_us
+        def __set__(self, val): self.this.sample_time_us = val
 
     property position:
         def __get__(self):
-            return [self.thisptr.position[i] for i in
+            return [self.this.position[i] for i in
                     range(cismrmrd.ISMRMRD_POSITION_LENGTH)]
         def __set__(self, val):
             for i in range(cismrmrd.ISMRMRD_POSITION_LENGTH):
-                self.thisptr.position[i] = val[i]
+                self.this.position[i] = val[i]
 
     property read_dir:
         def __get__(self):
-            return [self.thisptr.read_dir[i] for i in
+            return [self.this.read_dir[i] for i in
                     range(cismrmrd.ISMRMRD_DIRECTION_LENGTH)]
         def __set__(self, val):
             for i in range(cismrmrd.ISMRMRD_DIRECTION_LENGTH):
-                self.thisptr.read_dir[i] = val[i]
+                self.this.read_dir[i] = val[i]
 
     property phase_dir:
         def __get__(self):
-            return [self.thisptr.phase_dir[i] for i in
+            return [self.this.phase_dir[i] for i in
                     range(cismrmrd.ISMRMRD_DIRECTION_LENGTH)]
         def __set__(self, val):
             for i in range(cismrmrd.ISMRMRD_DIRECTION_LENGTH):
-                self.thisptr.phase_dir[i] = val[i]
+                self.this.phase_dir[i] = val[i]
 
     property slice_dir:
         def __get__(self):
-            return [self.thisptr.slice_dir[i] for i in
+            return [self.this.slice_dir[i] for i in
                     range(cismrmrd.ISMRMRD_DIRECTION_LENGTH)]
         def __set__(self, val):
             for i in range(cismrmrd.ISMRMRD_DIRECTION_LENGTH):
-                self.thisptr.slice_dir[i] = val[i]
+                self.this.slice_dir[i] = val[i]
 
     property patient_table_position:
         def __get__(self):
-            return [self.thisptr.patient_table_position[i] for i in
+            return [self.this.patient_table_position[i] for i in
                     range(cismrmrd.ISMRMRD_POSITION_LENGTH)]
         def __set__(self, val):
             for i in range(cismrmrd.ISMRMRD_POSITION_LENGTH):
-                self.thisptr.patient_table_position[i] = val[i]
+                self.this.patient_table_position[i] = val[i]
 
     property idx:
         def __get__(self): return self.idx
@@ -194,19 +194,19 @@ cdef class AcquisitionHeader:
 
     property user_int:
         def __get__(self):
-            return [self.thisptr.user_int[i] for i in
+            return [self.this.user_int[i] for i in
                     range(cismrmrd.ISMRMRD_USER_INTS)]
         def __set__(self, val):
             for i in range(cismrmrd.ISMRMRD_USER_INTS):
-                self.thisptr.user_int[i] = val[i]
+                self.this.user_int[i] = val[i]
 
     property user_float:
         def __get__(self):
-            return [self.thisptr.user_float[i] for i in
+            return [self.this.user_float[i] for i in
                     range(cismrmrd.ISMRMRD_USER_FLOATS)]
         def __set__(self, val):
             for i in range(cismrmrd.ISMRMRD_USER_FLOATS):
-                self.thisptr.user_float[i] = val[i]
+                self.this.user_float[i] = val[i]
 
 cdef class Acquisition:
     cdef cismrmrd.ISMRMRD_Acquisition *this
@@ -230,142 +230,142 @@ cdef class ImageHeader:
         free(self.this)
 
     property version:
-        def __get__(self): return self.thisptr.version
-        def __set__(self, val): self.thisptr.version = val
+        def __get__(self): return self.this.version
+        def __set__(self, val): self.this.version = val
 
     property flags:
-        def __get__(self): return self.thisptr.flags
-        def __set__(self, val): self.thisptr.flags = val
+        def __get__(self): return self.this.flags
+        def __set__(self, val): self.this.flags = val
 
     property measurement_uid:
-        def __get__(self): return self.thisptr.measurement_uid
-        def __set__(self, val): self.thisptr.measurement_uid = val
+        def __get__(self): return self.this.measurement_uid
+        def __set__(self, val): self.this.measurement_uid = val
 
     property matrix_size:
         def __get__(self):
-            return [self.thisptr.matrix_size[i] for i in range(3)]
+            return [self.this.matrix_size[i] for i in range(3)]
         def __set__(self, val):
             for i in range(3):
-                self.thisptr.matrix_size[i] = val[i]
+                self.this.matrix_size[i] = val[i]
 
     property field_of_view:
         def __get__(self):
-            return [self.thisptr.field_of_view[i] for i in range(3)]
+            return [self.this.field_of_view[i] for i in range(3)]
         def __set__(self, val):
             for i in range(3):
-                self.thisptr.field_of_view[i] = val[i]
+                self.this.field_of_view[i] = val[i]
 
     property channels:
-        def __get__(self): return self.thisptr.channels
-        def __set__(self, val): self.thisptr.channels = val
+        def __get__(self): return self.this.channels
+        def __set__(self, val): self.this.channels = val
 
     property position:
         def __get__(self):
-            return [self.thisptr.position[i] for i in
+            return [self.this.position[i] for i in
                     range(cismrmrd.ISMRMRD_POSITION_LENGTH)]
         def __set__(self, val):
             for i in range(cismrmrd.ISMRMRD_POSITION_LENGTH):
-                self.thisptr.position[i] = val[i]
+                self.this.position[i] = val[i]
 
     property read_dir:
         def __get__(self):
-            return [self.thisptr.read_dir[i] for i in
+            return [self.this.read_dir[i] for i in
                     range(cismrmrd.ISMRMRD_DIRECTION_LENGTH)]
         def __set__(self, val):
             for i in range(cismrmrd.ISMRMRD_DIRECTION_LENGTH):
-                self.thisptr.read_dir[i] = val[i]
+                self.this.read_dir[i] = val[i]
 
     property phase_dir:
         def __get__(self):
-            return [self.thisptr.phase_dir[i] for i in
+            return [self.this.phase_dir[i] for i in
                     range(cismrmrd.ISMRMRD_DIRECTION_LENGTH)]
         def __set__(self, val):
             for i in range(cismrmrd.ISMRMRD_DIRECTION_LENGTH):
-                self.thisptr.phase_dir[i] = val[i]
+                self.this.phase_dir[i] = val[i]
 
     property slice_dir:
         def __get__(self):
-            return [self.thisptr.slice_dir[i] for i in
+            return [self.this.slice_dir[i] for i in
                     range(cismrmrd.ISMRMRD_DIRECTION_LENGTH)]
         def __set__(self, val):
             for i in range(cismrmrd.ISMRMRD_DIRECTION_LENGTH):
-                self.thisptr.slice_dir[i] = val[i]
+                self.this.slice_dir[i] = val[i]
 
     property patient_table_position:
         def __get__(self):
-            return [self.thisptr.patient_table_position[i] for i in
+            return [self.this.patient_table_position[i] for i in
                     range(cismrmrd.ISMRMRD_POSITION_LENGTH)]
         def __set__(self, val):
             for i in range(cismrmrd.ISMRMRD_POSITION_LENGTH):
-                self.thisptr.patient_table_position[i] = val[i]
+                self.this.patient_table_position[i] = val[i]
 
     property average:
-        def __get__(self): return self.thisptr.average
-        def __set__(self, val): self.thisptr.average = val
+        def __get__(self): return self.this.average
+        def __set__(self, val): self.this.average = val
 
     property slice:
-        def __get__(self): return self.thisptr.slice
-        def __set__(self, val): self.thisptr.slice = val
+        def __get__(self): return self.this.slice
+        def __set__(self, val): self.this.slice = val
 
     property contrast:
-        def __get__(self): return self.thisptr.contrast
-        def __set__(self, val): self.thisptr.contrast = val
+        def __get__(self): return self.this.contrast
+        def __set__(self, val): self.this.contrast = val
 
     property phase:
-        def __get__(self): return self.thisptr.phase
-        def __set__(self, val): self.thisptr.phase = val
+        def __get__(self): return self.this.phase
+        def __set__(self, val): self.this.phase = val
 
     property repetition:
-        def __get__(self): return self.thisptr.repetition
-        def __set__(self, val): self.thisptr.repetition = val
+        def __get__(self): return self.this.repetition
+        def __set__(self, val): self.this.repetition = val
 
     property set:
-        def __get__(self): return self.thisptr.set
-        def __set__(self, val): self.thisptr.set = val
+        def __get__(self): return self.this.set
+        def __set__(self, val): self.this.set = val
 
     property acquisition_time_stamp:
-        def __get__(self): return self.thisptr.acquisition_time_stamp
-        def __set__(self, val): self.thisptr.acquisition_time_stamp = val
+        def __get__(self): return self.this.acquisition_time_stamp
+        def __set__(self, val): self.this.acquisition_time_stamp = val
 
     property physiology_time_stamp:
         def __get__(self):
-            return [self.thisptr.physiology_time_stamp[i] for i in
+            return [self.this.physiology_time_stamp[i] for i in
                     range(cismrmrd.ISMRMRD_PHYS_STAMPS)]
         def __set__(self, val):
             for i in range(cismrmrd.ISMRMRD_PHYS_STAMPS):
-                self.thisptr.physiology_time_stamp[i] = val[i]
+                self.this.physiology_time_stamp[i] = val[i]
 
     property image_data_type:
-        def __get__(self): return self.thisptr.image_data_type
-        def __set__(self, val): self.thisptr.image_data_type = val
+        def __get__(self): return self.this.image_data_type
+        def __set__(self, val): self.this.image_data_type = val
 
     property image_type:
-        def __get__(self): return self.thisptr.image_type
-        def __set__(self, val): self.thisptr.image_type = val
+        def __get__(self): return self.this.image_type
+        def __set__(self, val): self.this.image_type = val
 
     property image_index:
-        def __get__(self): return self.thisptr.image_index
-        def __set__(self, val): self.thisptr.image_index = val
+        def __get__(self): return self.this.image_index
+        def __set__(self, val): self.this.image_index = val
 
     property image_series_index:
-        def __get__(self): return self.thisptr.image_series_index
-        def __set__(self, val): self.thisptr.image_series_index = val
+        def __get__(self): return self.this.image_series_index
+        def __set__(self, val): self.this.image_series_index = val
 
     property user_int:
         def __get__(self):
-            return [self.thisptr.user_int[i] for i in
+            return [self.this.user_int[i] for i in
                     range(cismrmrd.ISMRMRD_USER_INTS)]
         def __set__(self, val):
             for i in range(cismrmrd.ISMRMRD_USER_INTS):
-                self.thisptr.user_int[i] = val[i]
+                self.this.user_int[i] = val[i]
 
     property user_float:
         def __get__(self):
-            return [self.thisptr.user_float[i] for i in
+            return [self.this.user_float[i] for i in
                     range(cismrmrd.ISMRMRD_USER_FLOATS)]
         def __set__(self, val):
             for i in range(cismrmrd.ISMRMRD_USER_FLOATS):
-                self.thisptr.user_int[i] = val[i]
+                self.this.user_int[i] = val[i]
 
 cdef class Image:
     cdef cismrmrd.ISMRMRD_Image *this
