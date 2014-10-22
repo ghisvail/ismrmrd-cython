01189b26eb468ebc668eaa2a10f5c977ffc4405d
Ghislain Antony Vaillant <ghisvail@gmail.com>
2014-10-22 16:45:48 +0100

    use cythonize

--- setup.py
+++ setup.py
@@ -1,23 +1,24 @@
 import os
 from distutils.core import setup
 from distutils.extension import Extension
-from Cython.Distutils import build_ext
+from Cython.Build import cythonize
 import numpy
 
 ismrmrd_home = os.environ['ISMRMRD_HOME']
 
-ext = Extension(
-    "ismrmrd",
-    ["cismrmrd.pxd", "ismrmrd.pyx"],
-    include_dirs=[os.path.join(ismrmrd_home, 'include'),
-                  numpy.get_include()],
-    library_dirs=[os.path.join(ismrmrd_home, 'lib')],
-    libraries=["ismrmrd"],
-    extra_link_args=[],
-)
+extensions = [
+	Extension(
+		"ismrmrd",
+		["ismrmrd.pyx"],
+		include_dirs=[os.path.join(ismrmrd_home, 'include'),
+					  numpy.get_include()],
+		library_dirs=[os.path.join(ismrmrd_home, 'lib')],
+		libraries=["ismrmrd"],
+		extra_link_args=[]
+	),
+]
 
 setup(
     name="ismrmrd",
-    ext_modules=[ext],
-    cmdclass={'build_ext':build_ext}
+	ext_modules = cythonize(extensions),
 )
